e1054161ab63f80211b58751c28f2543e02ba9ce
Neeraj <neeraj.messi10@gmail.com>
2025-02-22 00:18:47 +0530

    repeat the original req first to cover application's duplicate entry logic

--- src/main/java/org/example/MyFIrstHTTPHandler.java
+++ src/main/java/org/example/MyFIrstHTTPHandler.java
@@ -35,7 +35,6 @@ public class MyFIrstHTTPHandler implements HttpHandler {
     @Override
     public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent httpRequestToBeSent) {
         if(!this.hash.isEmpty() && httpRequestToBeSent.isInScope() && httpRequestToBeSent.toolSource().isFromTool(ToolType.PROXY) && httpRequestToBeSent.method().equals("POST")) {
-//          HttpRequest modifiedRequest = httpRequestToBeSent.withAddedHeader("TestHash", this.hash);
             return RequestToBeSentAction.continueWith(httpRequestToBeSent);
         }
         return null;
@@ -43,36 +42,22 @@ public class MyFIrstHTTPHandler implements HttpHandler {
 
     @Override
     public ResponseReceivedAction handleHttpResponseReceived(HttpResponseReceived httpResponseReceived) {
-        String input = "";
-        if(httpResponseReceived.initiatingRequest().isInScope() && httpResponseReceived.initiatingRequest().method().equals("POST")){
-            if(httpResponseReceived.hasHeader("Age")){
-                input = httpResponseReceived.headerValue("Age");
-            }
-            else if (httpResponseReceived.hasHeader("Date")){
-                input = httpResponseReceived.headerValue("Date");
-            }
-            else{
-                input = "Null";
-            }
-
-            try {
-                MessageDigest digest = MessageDigest.getInstance("SHA-256");
-                digest.update(input.getBytes(StandardCharsets.UTF_8));
-                this.hash = HexFormat.of().formatHex(digest.digest());
-                MAPI.getINSTANCE().logging().logToOutput("last generated hash: " + this.hash);
-                this.ui.setHashFieldTxt(this.hash); //updating the UI text field with the last generated hash
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
+        if(httpResponseReceived.initiatingRequest().isInScope() && httpResponseReceived.initiatingRequest().method().equals("POST") && httpResponseReceived.toolSource().isFromTool(ToolType.PROXY)){
             HttpRequest originalRequest = httpResponseReceived.initiatingRequest();
             HttpRequest modifiedRequest = originalRequest.withTransformationApplied(HttpTransformation.TOGGLE_METHOD);
+            //to cover scenarios where application does not allow duplicate entries ,
+            // for example - adding a course with same name. In this case, the original request will succeed, and the second time will fail because of app logic.
+            // So we repeat the original request one more time to make sure the response covers any duplicate checking logic and then send the modified request.
+            HttpRequestResponse originalRepeatedHttpRequestResponse = MAPI.getINSTANCE().http().sendRequest(originalRequest);
             HttpRequestResponse modifiedHttpRequestResponse = MAPI.getINSTANCE().http().sendRequest(modifiedRequest);
             MAPI.getINSTANCE().logging().logToOutput("\n\nMethod: " + originalRequest.method());
             MAPI.getINSTANCE().logging().logToOutput("\nOriginal Request:\n" + originalRequest);
             MAPI.getINSTANCE().logging().logToOutput("\nOriginal Response:\n" + httpResponseReceived);
+            MAPI.getINSTANCE().logging().logToOutput("\nOriginal Repeated Request:\n" + originalRepeatedHttpRequestResponse.request());
+            MAPI.getINSTANCE().logging().logToOutput("\nOriginal Repeated Response:\n" + originalRepeatedHttpRequestResponse.response());
             MAPI.getINSTANCE().logging().logToOutput("\nModified Request:\n" + modifiedHttpRequestResponse.request());
             MAPI.getINSTANCE().logging().logToOutput("\nModified Response:\n" + modifiedHttpRequestResponse.response());
-            String result = PtGUtils.analyzeResponse(httpResponseReceived, modifiedHttpRequestResponse.response());
+            String result = PtGUtils.analyzeResponse(originalRepeatedHttpRequestResponse.response(), modifiedHttpRequestResponse.response());
             if(result.equals("SAME")){
                 MAPI.getINSTANCE().siteMap().add(auditIssue(
                                 "PostToGet ",
